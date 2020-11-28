f4f907f5d050f3b8186a4871546587a53d2e30e0
Bhavna288 <bhavnatahelyani288@gmail.com>
2020-11-28 20:42:12 +0530

    Commit

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul>
+                <li>Projects</li>
+                <li>Skills</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
