07229583412c4c0f8fbd99e4325f65da0f5e412b
Bhavna288 <bhavnatahelyani288@gmail.com>
2020-12-01 14:15:01 +0530

    Changes rollback

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Nav() {
     return (
         <nav>
-            <h3>Logo</h3>
+            <Link to="/" className="logoTxt">
+                <p>B</p>
+            </Link>
             <ul className="nav-links">
-                <Link to="/projects"><li>Projects</li></Link>
+                <Link to="/">
+                    <FontAwesomeIcon icon={ ["fas", "bars"] } />
+                </Link>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/works"><li>Works</li></Link>
                 <Link to="/certificates"><li>Skills</li></Link>
             </ul>
         </nav>
