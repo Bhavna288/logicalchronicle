db0283d584b491535516d20dbc5e1084561031e1
Bhavna288 <bhavnatahelyani288@gmail.com>
2021-03-26 21:47:36 +0530

    Commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -17,8 +17,29 @@ function Nav() {
         }
     }
 
+    let listener = null
+    const [scrollState, setScrollState] = useState("top")
+
+    useEffect(() => {
+        listener = document.addEventListener("scroll", e => {
+            var scrolled = document.scrollingElement.scrollTop
+            if (scrolled >= 600) {
+                if (scrollState !== "no-top") {
+                    setScrollState("no-top")
+                }
+            } else {
+                if (scrollState !== "top") {
+                    setScrollState("top")
+                }
+            }
+        })
+        return () => {
+            document.removeEventListener("scroll", listener)
+        }
+    }, [scrollState]);
+
     return (
-        <nav>
+        <nav className={ scrollState }>
             <Link to="/" className="logoTxt">
                 <p>B</p>
             </Link>
